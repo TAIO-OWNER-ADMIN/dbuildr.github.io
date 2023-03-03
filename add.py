e8993fb652b8bd9b46652f721ed3d68b98a0eedc
TAIO-OWNER-ADMIN <120074745+TAIO-OWNER-ADMIN@users.noreply.github.com>
2023-03-03 13:04:26 +1100

    Update add.py

--- add.py
+++ add.py
@@ -2,6 +2,6 @@ import BeautifulSoup4
 soup = BeautifulSoup(html)
 inputs=soup.find("input", {"id": "input-id"})
 print inputs[0]['value']
-f = open("/caramelslices/code.html", "a")
+f = open("/caramelslices/index.html", "a")
 f.write(inputs)
 f.close()
