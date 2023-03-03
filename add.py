aa271c4ec9fcf2829cecd27caf80fc38795c393e
TAIO-OWNER-ADMIN <120074745+TAIO-OWNER-ADMIN@users.noreply.github.com>
2023-03-03 13:20:02 +1100

    Update add.py

--- add.py
+++ add.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
-def add:
-  f = open("/caramelslices/index.html", "a")
-  f.write()
-  f.close()
 driver = webdriver.Chrome(executable_path="caramelslices/index.html")
 driver.implicitly_wait(0.5)
-driver.get("https://dbuildrau.github.io/")
+driver.get("https://taio-owner-admin.github.io/dbuildr.github.io")
 #identify element
 l= driver.find_element_by_name("in")
 l.send_keys("in")
-#get_attribute() to get value of input box
-print("Value of input box: " + l.get_attribute('in'))
+x = l.get_attribute('in'))
 driver.close()
+
+def add():
+  f = open("/caramelslices/index.html", "a")
+  f.write(x)
+  f.close()
