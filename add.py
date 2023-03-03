aca2c6ced30c125a1bd1bf38fd5cddce9c40dacb
TAIO-OWNER-ADMIN <120074745+TAIO-OWNER-ADMIN@users.noreply.github.com>
2023-03-03 13:28:54 +1100

    Update add.py

--- add.py
+++ add.py
@@ -1,14 +1,1 @@
-from selenium import webdriver
-driver = webdriver.Chrome(executable_path="caramelslices/index.html")
-driver.implicitly_wait(0.5)
-driver.get("https://taio-owner-admin.github.io/dbuildr.github.io")
-#identify element
-l= driver.find_element_by_name("in")
-l.send_keys("in")
-x = l.get_attribute('in'))
-driver.close()
 
-def add():
-  f = open("/caramelslices/index.html", "a")
-  f.write(x)
-  f.close()
