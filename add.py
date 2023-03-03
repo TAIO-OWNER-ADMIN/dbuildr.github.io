7e3d49314d268b00816e03c9727a66b4c29d4365
TAIO-OWNER-ADMIN <120074745+TAIO-OWNER-ADMIN@users.noreply.github.com>
2023-03-03 13:11:01 +1100

    Update add.py

--- add.py
+++ add.py
@@ -1,7 +1,14 @@
-import BeautifulSoup4
-soup = BeautifulSoup(html)
-inputs=soup.find("input", {"id": "input-id"})
-print inputs[0]['value']
-f = open("/caramelslices/index.html", "a")
-f.write(inputs)
-f.close()
+from selenium import webdriver
+def add:
+  f = open("/caramelslices/index.html", "a")
+  f.write()
+  f.close()
+driver = webdriver.Chrome(executable_path="caramelslices/index.html")
+driver.implicitly_wait(0.5)
+driver.get("https://dbuildr")
+#identify element
+l= driver.find_element_by_name("q")
+l.send_keys("q")
+#get_attribute() to get value of input box
+print("Value of input box: " + l.get_attribute('value'))
+driver.close()
