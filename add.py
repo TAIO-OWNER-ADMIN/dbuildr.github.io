6ddf9a623149b89542a449b563e85c471a4b7489
TAIO-OWNER-ADMIN <120074745+TAIO-OWNER-ADMIN@users.noreply.github.com>
2023-03-03 13:13:57 +1100

    Update add.py

--- add.py
+++ add.py
@@ -5,10 +5,10 @@ def add:
   f.close()
 driver = webdriver.Chrome(executable_path="caramelslices/index.html")
 driver.implicitly_wait(0.5)
-driver.get("https://dbuildr")
+driver.get("https://dbuildrau.github.io/")
 #identify element
-l= driver.find_element_by_name("q")
-l.send_keys("q")
+l= driver.find_element_by_name("in")
+l.send_keys("in")
 #get_attribute() to get value of input box
-print("Value of input box: " + l.get_attribute('value'))
+print("Value of input box: " + l.get_attribute('in'))
 driver.close()
